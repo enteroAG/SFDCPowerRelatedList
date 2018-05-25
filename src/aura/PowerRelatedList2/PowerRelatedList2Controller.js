031fcc9606f053a7c9c9f9eac06ad29e5c4c81ad
fabian384 <Fabian.peters@entero.de>
2018-05-25 17:21:30 +0200

    Add files via upload

---
+++ src/aura/PowerRelatedList2/PowerRelatedList2Controller.js
@@ -0,0 +1,84 @@
+({
+    doInit : function(component, event, helper) {
+        console.log('recordId');
+        console.log(component.get("v.recordId"));
+        component.set("v.showSpinner", true);
+        //TODO: do a cleanup on displayFields parameter to make sure it's not invalid (commas, spaces, etc)        
+        //build the query
+        
+        //get the describe
+        helper.query(component, helper.buildQuery(component));
+        helper.getPlural(component);
+        //create placeholder record with only the lookup populated
+        helper.setNewRecord(component);
+    },
+    
+    filter: function (component, event, helper){
+        helper.filter(component);
+    },
+    
+    createRecord : function (component) {
+        //set the add value object
+        component.set("v.adding", true);        
+    },
+    
+    handleFieldChange : function (component, event){
+        //console.log("heard field change");
+        var localObject = component.get("v.newRecord");
+        var objectChange = event.getParam("object");
+        var newObject = _.merge(localObject, objectChange);
+        //console.log(newObject);
+        component.set("v.newRecord", _.merge(localObject, objectChange));
+        
+        //no debug version
+        //component.set("v.newRecord", _.merge(component.get("v.newRecord"), event.getParam("object")));
+                
+    },
+    
+    saveNewRecord : function (component, event, helper){
+        var action = component.get("c.create");
+
+        action.setParams({
+            "objtype" : component.get("v.objectName"), 
+            "fields" :JSON.stringify(component.get("v.newRecord"))
+        })
+        action.setCallback(self, function (a){         
+            
+            if (a.getState() === "SUCCESS") {
+                var response = JSON.parse(a.getReturnValue());
+                var results = component.get("v.results");
+                results.push(response.object);            
+                
+                component.set("v.results", results); //set results
+                helper.filter(component);
+                component.set("v.adding", false); //close the modal
+                helper.setNewRecord(component); //ready for next one
+
+            } else if (a.getState() === "ERROR"){ //fires toasts with any issues
+                var appEvent = $A.get("e.c:handleCallbackError");
+                appEvent.setParams({
+                    "errors" : a.getError(),
+                    "errorComponentName" : "PRL"
+                });
+                appEvent.fire(); 
+            }                        
+        });
+        $A.enqueueAction(action);
+        
+    },
+    
+    cancelCreate : function (component){
+        component.set("v.adding", false); 
+    },
+    
+    handleSelect : function (component, event){
+        //Here, I want to navigate to the record
+        //console.log("event");
+        //console.log(event);        
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+            "recordId": event.getParam("recordId")            
+        });
+        navEvt.fire();
+    }
+})
---
+++ src/aura/PowerRelatedList2/PowerRelatedList2Helper.js
@@ -0,0 +1,90 @@
+({
+    //shared by lots of functions.  You give it a comma-separated list of stuff, it returns a trimmed array
+    CSL2Array: function (CSL){
+        
+        try{
+            var outputArray = CSL.split(",");
+            _.forEach(outputArray, function (value, key){
+                outputArray[key] = _.trim(value);
+            });
+            return outputArray;
+        } catch(err){
+            //intended to handle the "CSL is null scenario"
+            return [];
+        }
+    },
+    
+    getPlural: function(component){
+        var action = component.get("c.whatsMyPlural");
+        //public static String whatsMyPlural(string objtype){
+        action.setParams({"objtype" : component.get("v.objectName")});
+        action.setCallback(self, function (a){
+            //console.log("plural returned!")
+            //console.log(a)
+            //console.log(a.getReturnValue);           
+            component.set("v.pluralLabel", a.getReturnValue());            
+        });
+        action.setStorable();
+        $A.enqueueAction(action);
+    },
+    
+    setNewRecord: function (component){
+        if (component.get("v.allowAdd")){
+            var record = {};
+            record[component.get("v.lookupField")] = component.get("v.recordId");            
+            component.set("v.newRecord", record);            
+        }        
+    },
+    
+    buildQuery: function (component){
+        var soql = "select Id, " + component.get("v.displayFields") + " from " + component.get("v.objectName") + " where " + component.get("v.lookupField") + " = '" + component.get("v.recordId") + "'";       
+        //console.log(soql);
+        return soql;
+    },
+    
+    query: function (component, soql){
+        var action = component.get("c.queryJSON");
+        action.setParams({"soql" : soql});
+        action.setCallback(self, function(a){
+            //console.log("query results"); 
+            var records = JSON.parse(a.getReturnValue())
+            //console.log(records);
+            component.set("v.results", records);
+            component.set("v.filteredResults", records); //initial unfiltered list
+            component.set("v.showSpinner", false);
+        });
+        $A.enqueueAction(action);        
+    },
+
+    filter : function (component){
+        var filter = component.get("v.filter");        
+        //console.log("in debounced function");
+        //console.log(filter);
+        if (!filter){
+            //console.log("no filter");
+            component.set("v.filteredResults", component.get("v.results"));    
+        } else {
+            //console.log("filter present: " + filter);
+            var goodStuff = _.filter(component.get("v.results"), function(record){
+                var contains = false;
+                _.forEach(record, function (value){
+                    contains = contains || _.includes(_.toString(value), filter);
+                });
+                return contains;
+            });
+            component.set("v.filteredResults", goodStuff);            
+        }
+        this.sort(component);
+    },
+    
+    //sort always occurs after filter
+    sort : function (component){
+        var sortState = component.get("v.sortState");
+        if (!sortState) return; //if it's not sorted, just skip it
+        var results = _.sortBy(component.get("v.filteredResults"), [sortState.field]);
+        if (sortState.direction === 'Descending'){
+            _.reverse(results);
+        }
+        component.set("v.filteredResults", results);
+    }
+})
